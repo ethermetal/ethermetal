7ba8f9ea14991a242140f64f3b1feea93b321752
Gershon Bialer <gershon.bialer@gmail.com>
2017-08-16 15:10:28 -0700

    Updated item description

--- items/0/send.js
+++ items/0/send.js
@@ -6,9 +6,9 @@ module.exports = function(callback) {
   WarehouseReceiptContract.deployed().then( (instance)=>{
           var act = "0x0027E07e7BF0ef7f89B1b3fE9b6F73657bBC3aD9";
           instance.addItem(act, 
-                           "1837 $5 PCGS AU55", 
+                           "1837 $5 US Gold Piece AU 55", 
           "https://gateway.ipfs.io/ipfs/QmW7qjBEg251HEhdsZSGQZd2WVuhCs3Uouv79MGLRW62WB\nhttps://gateway.ipfs.io/ipfs/QmXL2euJW6peommtB2nzPQpvPpwUFZQDNpMPqdiP7HLUby\nhttps://gateway.ipfs.io/ipfs/QmQfeZKZmNofsNpzuHkH45ac9U73Ueie4SAzBMHfNRyv6f"
-           , 0, 1533860142,532000000000000000,338000000000000000, {from: act, gas:900000}).then( (tr) => {
+           , 0, 1534457141,523412315974876000,332325279984048000, {from: act, gas:900000}).then( (tr) => {
                  console.log("Added coin");
       });
   });
